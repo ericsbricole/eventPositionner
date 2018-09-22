8b6b2f7d98c103a3bd56dfc6a1a41680aff4129d
ericsbricole <e.pennecot@laposte.net>
2018-09-22 12:58:48 +0200

    ajout d'une V1 de tableau

--- eventPositionner.js
+++ eventPositionner.js
@@ -62,18 +62,23 @@ function drawMap(parsedJson){
         .enter()
         .append('path')
         .attr('d', geoGenerator)
-        .attr("id", function(d, i) {
-		return "country" + d.properties.NAME;
-	    })
-        .attr("class", "country");
+        .attr("id", (d) => "sov" + d.properties.SOVEREIGNT + d.properties.NAME)
+		.attr("class", "sov")
+		.attr("name", (d) => d.properties.NAME)
+		.attr("cont", (d) => d.properties.CONTINENT);//TODO: name and cont might be simpler to set here.
     
     //TODO?: get NAME and modify style of hovered object only once into a mouseover, then call the .on("mousemove"? Might be more optimized.
-    countries.on("mousemove", function(data){
+	var tooltipText;
+	countries.on("mousemove", function(d){
 	    //The tooltip has to track the mouse and change its style/content.
 	    mouse = d3.mouse(this);
 	    x = mouse[0];
-	    y = mouse[1];
-		tooltip.html(data.properties.NAME)
+		y = mouse[1];
+		if (d.properties.SOVEREIGNT !== d.properties.NAME)
+			tooltipText = d.properties.SOVEREIGNT + "</br>(" + d.properties.NAME+")"
+		else
+		tooltipText = d.properties.SOVEREIGNT + "</br>";
+		tooltip.html(tooltipText)
 			.style("left", d3.event.pageX-400 + "px")
 			.style("top", d3.event.pageY-150 + "px")
 			.attr("width", 200)
@@ -81,7 +86,7 @@ function drawMap(parsedJson){
 	    tooltip.transition()
 			.duration(100)
 			.style("opacity", 1);
-	    //the country also has to change its style to make it clear it is under observation.
+	    //the sov also has to change its style to make it clear it is under observation.
 	    //TODO: find another change of style which would be more stylish (coloration change?)
 	    d3.select(this).transition()
 			.duration(200)
@@ -97,23 +102,27 @@ function drawMap(parsedJson){
 		.attr("opacity", 1);
 	})
 
-	var clickedCountry;
+	var clickedSov;
+	var clickedName;
 	var table = $(".table");
-	countries.on("click",function(data){
-		clickedCountry = data.properties.NAME
+	countries.on("click",function(d,i,li){
+		clickedSov = d.properties.SOVEREIGNT;
+		clickedName = d.properties.NAME
 		if (table.children().length === 0){
-			var thCountry = "<th>Pays:</th>";
+			var thSov = "<th>Souveraineté:</th>";
+			var thName = "<th>Nom:</th>";
 			var thNumber = "<th>Nombre:</th>";
-			table.append("<thead><tr>"+ thCountry + thNumber +"</tr></thead><tbody id='tableBody'></tbody>");
+			table.append("<thead><tr>" + thSov + thName + thNumber +"</tr></thead><tbody id='tableBody'></tbody>");
 		}
-		if ($("td#" + data.properties.NAME).length <1){
-			var tdCountry ="<td id="+data.properties.NAME+">"+data.properties.NAME+"</td>";
-			var tdCountryNumber ="<td id="+data.properties.NAME+"Counter>"+1+"</td>" 
-			$("#tableBody").append("<tr>"+tdCountry+tdCountryNumber+"</tr>");			
+		if ($("td[data-name='"+clickedName+"']").length <1){
+			var tdSov = "<td data-sov='"+clickedSov+"' data-name='"+clickedName+"'>"+clickedSov+"</td>";
+			var tdName = "<td data-sov='"+clickedSov+"' data-name='"+clickedName+"'>"+clickedName+"</td>";
+			var tdSovNumber ="<td data-sov='"+clickedSov+"' data-name='"+clickedName+"'>"+1+"</td>" 
+			$("#tableBody").append("<tr>"+tdSov+tdName+tdSovNumber+"</tr>");			
 		}
 		else{
-			var currentValue = Number($("#"+data.properties.NAME+"Counter").text());
-			$("#"+data.properties.NAME+"Counter").text(currentValue+1);
+			var currentValue = parseInt($("td[data-name="+clickedName+"]").last().text());
+			$("td[data-name="+clickedName+"]").last().text(currentValue+=1);
 		}
 		//after updating the table of counters, we update colorations as well
 		mapColoration();
@@ -128,6 +137,5 @@ function drawMap(parsedJson){
 		counters.each(function(k,v){
 			countersObject[v.id.split("Counter")[0]] = v.innerHTML;
 		});
-		countersObject
 	}
 }
